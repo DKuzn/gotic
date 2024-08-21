a03b871e4002b7234bf38c7adb1ff1cdc85ad147
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2024-08-21 17:28:43 +0300

    Update to use flags

--- gotic.go
+++ gotic.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/spf13/cobra"
 )
 
-func main() {
+var dir string
+var port int
+
+var rootCmd = &cobra.Command{
+	Use:  "gotic",
+	Long: "A simple static server written in Go.",
+	Run:  serveStatic,
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&dir, "dir", "d", "./static/", "The directory is used to store static files.")
+	rootCmd.Flags().IntVarP(&port, "port", "p", 8080, "The port is used to listen requests.")
+}
+
+func serveStatic(cmd *cobra.Command, args []string) {
 	r := mux.NewRouter()
-	fs := http.FileServer(http.Dir("./static/"))
+	fs := http.FileServer(http.Dir(dir))
 
 	r.PathPrefix("/").Handler(http.StripPrefix("/", fs))
 
 	methods := handlers.AllowedMethods([]string{"POST", "GET", "PUT", "DELETE"})
 	origins := handlers.AllowedOrigins([]string{"*"})
-	http.ListenAndServe(":8080", handlers.CORS(origins, methods)(r))
+	http.ListenAndServe(fmt.Sprintf(":%d", port), handlers.CORS(origins, methods)(r))
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
