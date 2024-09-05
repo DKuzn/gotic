9ff2b8f85eb2af935a35a1348b42c07bdc4ae766
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2024-09-05 19:40:55 +0300

    Add logging middleware

--- gotic.go
+++ gotic.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -26,18 +27,28 @@ func init() {
 
 func serveStatic(cmd *cobra.Command, args []string) {
 	r := mux.NewRouter()
+	r.Use(loggingMiddleware)
 	fs := http.FileServer(http.Dir(dir))
 
 	r.PathPrefix("/").Handler(http.StripPrefix("/", fs))
 
 	methods := handlers.AllowedMethods([]string{"POST", "GET", "PUT", "DELETE"})
 	origins := handlers.AllowedOrigins([]string{"*"})
+
+	log.Printf("gotic server is running on port: %d", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), handlers.CORS(origins, methods)(r))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.RemoteAddr, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		os.Exit(1)
 	}
 }
